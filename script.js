b6103140b7ec006f689bec80a512ccb27e62fb3e
Shruti Singh <109094029+Shrutii08@users.noreply.github.com>
2022-08-24 12:47:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const renderCountry = function(data)
+{
+
+const html = ` 
+ <article class="country">
+<img class="country__img" src="${data.flag}"/>
+<div class="country__data">
+  <h3 class="country__name">"${data.name}"</h3>
+  <h4 class="country__region">${data.region}"</h4>
+  <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} people</p>
+  <p class="country__row"><span>🗣️</span>${data.languages}</p>
+  <p class="country__row"><span>💰</span>${data.currencies}</p>
+</div>
+</article>
+`;
+countriesContainer.insertAdjacentHTML('beforeend',html);
+countriesContainer.style.opacity = 1;
+};
+// ajax  call country 1
+    const getCountryAndNeighbour = function (country){
+const request = new XMLHttpRequest();
+request.open('GET',`https://restcountries.com/v3.1/name/${country}`);
+ request.send();
+ 
+request.addEventListener('load',function(){
+const [data] = JSON.parse(this.responseText);
+console.log(data);
+//render country
+renderCountry(data);
+
+// get nighbour country
+const [neighbour]  = data.borders;
+});  
+};
+getCountryAndNeighbour('portugal');
